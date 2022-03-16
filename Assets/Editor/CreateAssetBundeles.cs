d5280f2eda1c5728cccb3ab87ee2859d9aef4beb
Anarrikot <Meetyanski@mail.ru>
2022-03-16 18:02:41 +0300

    test

---
+++ Assets/Editor/CreateAssetBundeles.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public class CreateAssetBundeles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
