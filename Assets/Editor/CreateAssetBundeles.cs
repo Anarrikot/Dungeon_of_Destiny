08ec85c39f3909350e3beb492f07b7703e46fc12
Anarrikot <Meetyanski@mail.ru>
2022-03-16 17:20:53 +0300

    test commit

---
+++ Assets/Editor/CreateAssetBundeles.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+
+public class CreateAssetBundels
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
+
---
+++ Assets/Scripts/Core/ManadgerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManadgerController : MonoBehaviour
+{
+    private string bundleURL = "";
+    private int version = 0;
+
+    [SerializeField] SpriteRenderer sRenderer;
+
+    public void Start()
+    {
+        StartCoroutine(DownloadAndCache());
+    }
+
+    IEnumerator DownloadAndCache()
+    {
+        while (!Caching.ready)
+            yield return null;
+
+        var www = WWW.LoadFromCacheOrDownload(bundleURL, version);
+        yield return www;
+
+        if(!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        Debug.Log("Бандл загружен!");
+        var assetBundle = www.assetBundle;
+        string spriteName = "Head";
+
+        var spriteRequest = assetBundle.LoadAssetAsync(spriteName, typeof(Sprite));
+        yield return spriteName;
+        Debug.Log("Изображение распаковано!");
+
+        sRenderer.sprite = spriteRequest.asset as Sprite;
+    }
+}
