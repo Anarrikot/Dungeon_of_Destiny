2160dbd784b741c63d6861ab43512ae77fd8f918
Алексей Митянский <meetyanski@mail.ru>
2022-03-24 19:52:34 +0300

    Commit_add_sprites

--- Assets/Scripts/Core/WindowsController.cs
+++ Assets/Scripts/Core/WindowsController.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class WindowsController 
 {
     private GameObject myPrefab;
+
     public void OpenWindow(string name)
     {
         myPrefab = (GameObject)Resources.Load(name);
         Main.Instantiate(myPrefab, Main.instance.windowCanvas.transform);
     }
-
-
 }
